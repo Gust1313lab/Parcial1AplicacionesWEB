2550f969ea0fbeb1154d4a32609214fbd3b441af
Luis Castillo <alanmira68@gmail.com>
2026-02-23 19:38:08 -0600

    [UPDATE] component login validators

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,16 +5,39 @@ import { AuthService } from '../../services/auth.service';
   selector: 'app-login',
   styleUrls: ['./login.component.scss'],
   template: `
-    <button (click)="login()">Login</button>
-  `
+  <h1>Login</h1>
+
+  <input placeholder="Usuario" #user>
+  <input type="password" placeholder="Password" #pass>
+
+  <button (click)="login(user.value, pass.value)">Login</button>
+
+  <p *ngIf="errorMsg" style="color:red">
+    {{ errorMsg }}
+  </p>
+`
 })
 export class LoginComponent {
+  errorMsg = '';
 
   constructor(private authService: AuthService) {}
 
-  login() {
-    this.authService.login('admin', '1234')
-      .subscribe(res => console.log(res));
+  login(username: string, password: string) {
+
+  //Validación
+  if (!username || !password) {
+    this.errorMsg = 'Debe ingresar usuario y contraseña';
+    return;
   }
+
+  //limpiar error si todo está bien
+  this.errorMsg = '';
+
+  this.authService.login(username, password)
+    .subscribe({
+      next: res => console.log('UI', res),
+      error: err => this.errorMsg = 'Credenciales incorrectas'
+    });
+}
 
 }
